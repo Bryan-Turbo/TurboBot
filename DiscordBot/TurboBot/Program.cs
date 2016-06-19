6c824a6d0e5323fe7145b3af5eb024d7af03bedd
Bryan Kroesbeek <BryanK.B96@gmail.com>
2016-06-19 21:05:04 +0200

    First set up.

---
+++ DiscordBot/TurboBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using TurboBot.BotLogic;
+
+namespace TurboBot {
+    class Program {
+        static void Main(string[] args) {
+            new Bot(new InitialBot()).Run();
+        }
+    }
+}
