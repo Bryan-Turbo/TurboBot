f108dcc2790b5c452b63b568f8e146b42ed3120d
Bryan Kroesbeek <BryanK.B96@gmail.com>
2017-05-23 21:19:59 +0200

    Minor Changes

---
+++ DiscordBot/BotExtension/VolumeHandler.fs
@@ -0,0 +1,11 @@
+﻿module VolumeHandling
+
+open System
+
+let private ConvertVolume = fun convertedBytes percentage -> ((percentage / 100.0) * (float(convertedBytes)))
+
+let private ConvertShortToBytes = fun short -> BitConverter.GetBytes(int16(short))
+
+let private ConvertBytesToShort = fun bytes -> BitConverter.ToInt16(bytes)
+
+let ChangeVolumeByPercentage = fun bytes percentage -> (ConvertShortToBytes ((ConvertVolume (ConvertBytesToShort bytes)) percentage))
