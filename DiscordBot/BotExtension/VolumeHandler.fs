d321d6f502740953d122a105754fce9356403b7a
Bryan Kroesbeek <BryanK.B96@gmail.com>
2017-05-24 14:07:24 +0200

    Removed redundant brackets

--- DiscordBot/BotExtension/VolumeHandler.fs
+++ DiscordBot/BotExtension/VolumeHandler.fs
@@ -2,10 +2,10 @@
 
 open System
 
-let private ConvertVolume = fun convertedBytes percentage -> ((percentage / 100.0) * (float(convertedBytes)))
+let private ConvertVolume = fun convertedBytes percentage -> (percentage / 100.0) * (float(convertedBytes))
 
 let private ConvertShortToBytes = fun short -> BitConverter.GetBytes(int16(short))
 
 let private ConvertBytesToShort = fun bytes -> BitConverter.ToInt16(bytes)
 
-let ChangeVolumeByPercentage = fun bytes percentage -> (ConvertShortToBytes ((ConvertVolume (ConvertBytesToShort bytes)) percentage))
+let ChangeVolumeByPercentage = fun bytes percentage -> ConvertShortToBytes ((ConvertVolume (ConvertBytesToShort bytes)) percentage)
